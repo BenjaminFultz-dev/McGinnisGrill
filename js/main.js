a9766722c1f7a6ee8ebbe594e924a4dd259f3662
Benjamin Fultz <xsymbiotex@gmail.com>
2021-06-22 15:28:11 -0400

    here we go now

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  const nav = document.getElementById("myTopnav");
+  const navButtons = nav.getElementsByClassName("navButton");
+
+  for (let i = 0; i < navButtons.length; i++) {
+      navButtons[i].addEventListener("click", function() {
+          let current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+      })
+  }
